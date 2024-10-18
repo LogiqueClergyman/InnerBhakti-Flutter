a596db2f169b0a244d9c91fa89becef8f61cefc7
LogiqueClergyman <adityavishwanathanverma@gmail.com>
2024-10-18 23:24:51 +0530

    pushing vercel.json

--- server/model/model.js
+++ server/model/model.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const ProgramSchema = new mongoose.Schema({
   name: String,
   image: String,
+  desc: String,
   tracks: [
     {
       name: String,
+      desc: String,
+      image: String,
       audioUrl: String,
     },
   ],
